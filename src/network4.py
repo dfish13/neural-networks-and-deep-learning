9dc8ab77e60a0c4309d073784d77b69e07b6caa8
Duncan Fisher <duncanfisher@Duncans-MacBook-Pro.local>
2019-06-14 11:46:15 -0400

    keras network

---
+++ src/network4.py
@@ -0,0 +1,39 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+
+batch_size = 10
+num_classes = 10
+epochs = 5
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+
+model.add(Dense(30, activation='sigmoid', input_shape=(784,)))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.mean_squared_error,
+              optimizer=keras.optimizers.SGD(lr=3.0),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1)
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
